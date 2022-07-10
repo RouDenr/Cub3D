3f198451fb8fc48223ef3913886287bd596f7f81
decordel <decordel@student.21-school.ru>
2022-07-10 22:51:16 +0300

    fin main part

--- srcs/mlx_init.c
+++ srcs/mlx_init.c
@@ -6,7 +6,7 @@
 /*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 00:25:18 by decordel          #+#    #+#             */
-/*   Updated: 2022/07/08 01:05:40 by decordel         ###   ########.fr       */
+/*   Updated: 2022/07/10 22:50:10 by decordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,8 +79,8 @@ t_mlx	game_init(t_map *map)
 	mlx.player = player_init(map->map_arr);
 	mlx_hook(mlx.win, 17, 0L, exit_hook, &mlx);
 	mlx_hook(mlx.win, 2, 1L << 0, key_hook, &mlx);
-	// mlx_hook(mlx.win, 6, 1L << 0, mouse_hook, &mlx);
-	// mlx_mouse_hide();
+	mlx_hook(mlx.win, 6, 1L << 0, mouse_hook, &mlx);
+	mlx_mouse_hide();
 	mlx_loop_hook(mlx.init, do_next_frame, &mlx);
 	mlx_loop(mlx.init);
 	return (mlx);
