728620e42092f87176cabb8ea20c1d25861c101d
decordel <decordel@student.21-school.ru>
2022-07-12 17:49:40 +0300

    fix: minimap

--- srcs/minimap.c
+++ srcs/minimap.c
@@ -6,18 +6,18 @@
 /*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 22:24:43 by decordel          #+#    #+#             */
-/*   Updated: 2022/07/12 00:24:03 by decordel         ###   ########.fr       */
+/*   Updated: 2022/07/12 17:40:57 by decordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/cub3D.h"
 
 #ifndef FT_W_MINIMAP
-# define FT_W_MINIMAP 200
+# define FT_W_MINIMAP 300
 #endif
 
 #ifndef FT_H_MINIMAP
-# define FT_H_MINIMAP 200
+# define FT_H_MINIMAP 300
 #endif
 
 #ifndef FT_X_MINIMAP
@@ -41,26 +41,30 @@ void	draw_minimap(t_mlx *mlx, t_map *map)
 {
 	int		i;
 	int		j;
+	int		temp = 1;
 	float	x_map;
 	float	y_map;
 	char	c;
 
-	x_map = 1.f * FT_W_MINIMAP / ft_strlen(*map->map_arr);
-	y_map = 1.f * FT_H_MINIMAP / ft_strmaslen(map->map_arr);
+	x_map = 1.f * ft_strlen(*map->map_arr);
+	while (x_map * temp <= FT_W_MINIMAP)
+		temp++;
+	x_map = 1.f * ft_strlen(*map->map_arr) * --temp;
+	y_map = 1.f * ft_strmaslen(map->map_arr) * temp;
 	printf("%f %f\n", x_map, y_map);
 	i = FT_X_MINIMAP;
-	while (i < FT_W_MINIMAP + FT_X_MINIMAP)
+	while (i < x_map + FT_X_MINIMAP)
 	{
 		j = FT_Y_MINIMAP;
-		while (j < FT_H_MINIMAP + FT_Y_MINIMAP)
+		while (j < y_map + FT_Y_MINIMAP)
 		{
-			c = map->map_arr[(int)((j - FT_Y_MINIMAP) / y_map)]
-			[(int)((i - FT_X_MINIMAP) / x_map)];
+			c = map->map_arr[(int)((j - FT_Y_MINIMAP) / temp)]
+			[(int)((i - FT_X_MINIMAP) / temp)];
 			if (c == '1')
 				ft_pixel_put(&mlx->screen, i, j, 0x000000);
 			else if (is_map_player_pos(&mlx->player,
-					(int)((i - FT_X_MINIMAP) / x_map),
-				(int)((j - FT_Y_MINIMAP) / y_map)))
+					(int)((i - FT_X_MINIMAP) / temp),
+				(int)((j - FT_Y_MINIMAP) / temp)))
 				ft_pixel_put(&mlx->screen, i, j, 0xff0000);
 			j++;
 		}
