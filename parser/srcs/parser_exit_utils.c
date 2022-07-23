36ddec8936abe187af709edac81b16ed5d2ddcd7
BoutPure <89292794+BoutPure@users.noreply.github.com>
2022-07-23 21:10:20 +0300

    feat: add cleanup functions

--- parser/srcs/parser_exit_utils.c
+++ parser/srcs/parser_exit_utils.c
@@ -6,7 +6,7 @@
 /*   By: vseel <vseel@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 12:13:10 by vseel             #+#    #+#             */
-/*   Updated: 2022/06/22 22:20:16 by vseel            ###   ########.fr       */
+/*   Updated: 2022/07/23 20:26:01 by vseel            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,47 @@ int	throw_error(char *msg, int exit_code, char mode)
 	if (mode == 'p' && msg)
 		perror(msg);
 	return (exit_code);
+}
+
+char	double_arr_free(char **strs)
+{
+	char	*to_free;
+	char	**to_free_head;
+
+	if (!strs)
+		return (1);
+	to_free_head = strs;
+	while (*strs)
+	{
+		to_free = *strs;
+		++strs;
+		free(to_free);
+	}
+	free(to_free_head);
+	return (1);
+}
+
+void	destroy_dyarr_struc(void *elem)
+{
+	if (!elem)
+		return ;
+	free(((t_dyarr *) elem)->line);
+	((t_dyarr *) elem)->line = 0;
+}
+
+char	map_free(t_map *map)
+{
+	free(map->wall_no);
+	free(map->wall_so);
+	free(map->wall_we);
+	free(map->wall_ea);
+	map->wall_no = 0;
+	map->wall_so = 0;
+	map->wall_we = 0;
+	map->wall_ea = 0;
+	double_arr_free(map->map_arr);
+	map->map_arr = 0;
+	ft_lstclear_dyarr(&map->map_arr_l, destroy_dyarr_struc);
+	map->map_arr_l = 0;
+	return (1);
 }
